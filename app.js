2c7febebc5098f7c76a243da30e7065a81d43e3b
sabu sabu <godugubalesabu@gmail.com>
2017-11-19 17:51:16 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
-
+app.get('/',function(req,resp){
+  console.log(resp);
+  console.log(req);
+  resp.send('Hello, World!');
+});
 
 module.exports = app
