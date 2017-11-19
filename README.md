b59c7bf1b9a93ad21b1fba5e1438244979ebd551
aviflombaum <avi@flombaum.com>
2017-11-19 17:03:55 -0500

    Tests yay

--- README.md
+++ README.md
@@ -2,14 +2,21 @@
 
 ## Objectives
 
-1. Use EJS to dynamically build an HTML Response
+1. Draw a route to a GET '/'
+2. Render `views/index.ejs` as a response.
+3. Pass data from the route callback to the view.
+4. Use data from the route callback in the view.
 
 ## Instructions
-
-This lab is incomplete and has no tests. 
 
 Just to be safe, before starting the tests or the server, run `npm install` in your terminal.
 
 You can run the tests with `learn`.
 
 To start your application, type `npm start`. The IDE will give you an IP address and port for your application, locally, your application will be running at `http://localhost:3000`.
+
+### GET '/' Route
+
+In `app.js`, define a route for the root of the application at `/`. In the callback for that route, you will be rendering a template inside the `views` directory called `index.ejs` using `resp.render()`.
+
+Your route callback should also pass a javascript object to the view with a key `welcomePhrase` that has a value of `Hello, World from Express and EJS!`.
--- test/appTest.js
+++ test/appTest.js
@@ -8,34 +8,62 @@ const chai = require('chai')
 const chaiHttp = require('chai-http');
 const app = require('../server');
 const sinon = require('sinon');
-const ejs = require('ejs');
 
 chai.use(chaiHttp);
 
 describe("app.js", function(){
   describe('/', function(){
-    it('responds with a 200', function(done){
-      chai.request(app)
-        .get("/")
-        .end(function(err, res){          
-          expect(res).to.have.status(200);
-          done();
-        });
-    });    
-
-    it('renders the index.ejs view', function(done){
-      const spy = sinon.spy(ejs, '__express');
-      
+    it('should render the "index" view', function(done) {
+      const spy = sinon.spy(app, 'render');
       
       chai.request(app)
         .get("/")
         .end(function(err, res){
-          console.log(res)
-          expect(spy.calledWithMatch(/index/)).to.be.true;
+          const viewRendered = spy.getCall(0).args[0];
+      
+          expect(viewRendered).to.be.eql('index');
+      
           spy.restore();
-
-          done();
+          done()
         });
-    });
+    })
+    
+    it('passes an object to the view with a key "welcomePhrase" and value "Hello, World from Express and EJS!"', function(done) {
+      const spy = sinon.spy(app, 'render');
+      
+      chai.request(app)
+        .get("/")
+        .end(function(err, res){
+          const viewData = spy.getCall(0).args[1];
+      
+          expect(viewData.welcomePhrase).to.be.eql("Hello, World from Express and EJS!");
+      
+          spy.restore();
+          done()
+        });
+    })
+
+    it('uses embeded <%= %> ejs to output the welcomePhrase in the template"', function(done) {
+      // const spy = sinon.spy(app, 'render');
+      const fs = require('fs')
+      fs.readFile('./views/index.ejs', 'utf8', function (err,data) {
+        expect(data).to.contain("<%=");
+        expect(data).to.contain("%>");
+        expect(data).to.contain("welcomePhrase");
+            
+        done()
+      });          
+    })
+
+    it('includes the welcomePhrase in the HTML response of the template"', function(done) {      
+      chai.request(app)
+        .get("/")
+        .end(function(err, res){      
+          expect(res.text).to.contain("Hello, World from Express and EJS!");
+            
+          done()
+        });
+    })
+
   });  
 });
