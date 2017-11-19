a37a3f9919a13f37c36dea5c075ff053b99c58bf
Dave Kokandy <drkokandy@gmail.com>
2017-11-19 18:11:46 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
-
+app.get('/', function(req, resp) {
+  const welcomeText = "Welcome to HTML";
+  resp.render('index', {"welcomeText": welcomeText})
+} )
 
 module.exports = app
