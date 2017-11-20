6a841d62241fcc2332970d91c5840e708c0cee29
harenarium <carolinel2610@gmail.com>
2017-11-20 23:04:07 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
-
+app.get("/",function(req, resp){
+  resp.render("index.ejs" ,{
+    data:"passing this data"
+  })
+})
 
 module.exports = app
