a869f57ee61e00ca3ad0479f40529ca0754a416d
Alishaan Asaria <alishaan.asaria@gmail.com>
2017-11-20 02:29:20 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
-
-
+app.get('/', function (req,res) {
+  res.render('index', {
+    welcomePhrase :"Hello, World from Express and EJS!"
+  })
+})
 module.exports = app
