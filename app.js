73c50ec08cd638ba82658c3db2c7c656cc3ca94e
Rebecca Inge <rebecca.inge@gmail.com>
2017-11-20 14:56:21 +0000

    Done.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express') //loading a package
+var app = express()  //defining a variable
 
 // Build Your Route Here
+app.get('/', function(req, resp){  //callback controller action (load data)
+  const welcomePhrase = `Hello, World from Express and EJS!`
+
+  resp.render("index", {
+    "welcomePhrase": welcomePhrase,
+    name: "Becky",
+    }) //new variable available, pass data from controller action into view
+})
 
 
 module.exports = app
