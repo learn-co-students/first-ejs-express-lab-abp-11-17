f35547ce5270a2bb8f0e223a39635c1c44d8fcfd
jencekade <gaephuoc@gmail.com>
2017-11-20 10:35:29 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+const welcomePhrase = "Hello, World from Express and EJS!";
+const myJobs = ["Barista", "Pole Aerialist"];
 
+app.get("/", function(req, resp) {
+  resp.render("index", {
+    welcomePhrase: welcomePhrase,
+    name: "Jence Kade",
+    myJobs: myJobs
+  });
+});
 
 module.exports = app
