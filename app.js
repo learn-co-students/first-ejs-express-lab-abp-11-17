4542f412819bd903955423db524954823a1e3c66
Maverick Tesini <mavericktesini@gmail.com>
2017-11-21 00:33:35 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
-
+let obj1 = {welcomePhrase: "Hello MOTO!"};
+app.get("/", function (req, res){
+  res.render("index", {welcomePhrase: "Hello, World from Express and EJS!"} );
+})
 
 module.exports = app
