77921488b1ba8523fa28edf4f027fc369ba63a30
Beth Schofield <bethmschofield@gmail.com>
2017-11-23 23:58:54 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+// app.get ('/', function(req, resp){
+//   resp.send ("whatup")
+// });
+
+app.get ('/', function(req, resp){
+  const welcomePhrase = "Hello, World from Express and EJS!"
+  resp.render("index", {
+    welcomePhrase: welcomePhrase,
+    name: "Beti",
+    hisName: "Aki"
+  })
+});
 
 
 module.exports = app
