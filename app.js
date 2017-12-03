7d810a9375fe5ca04517f069e66bd3636b0766be
Ashton Reed <dreamergirl411@gmail.com>
2017-12-03 19:46:19 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+app.get('/', function(req,resp){
+  const welcomePhrase = 'Hello, World from Express and EJS!'
 
+  resp.render('index', {'welcomePhrase': welcomePhrase})
+})
 
 module.exports = app
