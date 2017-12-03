9a7f91ade11b22221866be2351051e09258bf042
Obi <obinnawalker@gmail.com>
2017-12-03 18:26:12 +0000

    Done.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express')
 var app = express()
+const path = require('path')
 
 // Build Your Route Here
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.get('/', function(req, resp){
+   const data = 'Hello, World from Express and EJS!'
+   resp.render('index', {"welcomePhrase": data})
+
+})
 
 module.exports = app
