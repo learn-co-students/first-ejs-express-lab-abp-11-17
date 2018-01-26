b2dd0166b9c00f84b4dd73bff8bc286c4389821a
carlitos321 <ca.cabrera321@gmail.com>
2018-01-26 03:22:10 +0000

    Done.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require('express')
 var app = express()
+const path = require('path');
 
-// Build Your Route Here
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, resp){
+  const welcomePhrase = "Hello, World from Express and EJS!"
+  const viewData = {
+    welcomePhrase: welcomePhrase
+  }
+
+  resp.render('index', viewData)
+});
 
 
 module.exports = app
