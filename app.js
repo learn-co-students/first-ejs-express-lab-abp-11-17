d267cd1073444e5ef5d34eb92271ffa69b5cfd58
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-02-07 18:54:20 +0000

    Done.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var express = require('express')
 var app = express()
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, resp){
+
+  resp.render('index',{welcomePhrase: "Hello, World from Express and EJS!"})
+})
 
 // Build Your Route Here
 
